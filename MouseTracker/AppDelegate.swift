22b59ba3add7b98f578dbd9dd42d13dbe9161244
Lars Kroll <lkroll@kth.se>
2019-08-13 20:06:27 +0200

    Initial Commit

---
+++ MouseTracker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MouseTracker
+//
+//  Created by Lars Kroll on 2019-08-13.
+//  Copyright © 2019 Lars Kroll. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
